ade5329dc2c41ac2ed611562c3e43c5ee3936f32
ndtn97 <32271862+ndtn97@users.noreply.github.com>
2022-09-24 20:06:22 +0900

    wip

--- js/background.js
+++ js/background.js
@@ -1,91 +1,101 @@
-chrome.runtime.onInstalled.addListener(async () => {
-    console.log("installed!")
-});
-
-function enable(tabId) {
-    chrome.declarativeNetRequest.updateSessionRules(
-        {
-            "removeRuleIds": [2],
-            "addRules": [
-                {
-                    "id": 1,
-                    "action": {
-                        "type": "redirect",
-                        "redirect": {
-                            "transform": {
-                                "queryTransform": {
-                                    "addOrReplaceParams": [
-                                        {
-                                            "key": "gl",
-                                            "value": "us"
-                                        },
-                                        {
-                                            "key": "hl",
-                                            "value": "en"
-                                        }
-                                    ]
-                                }
-                            }
+rule =
+{
+    "id": 2,
+    "action": {
+        "type": "redirect",
+        "redirect": {
+            "transform": {
+                "queryTransform": {
+                    "addOrReplaceParams": [
+                        {
+                            "key": "gl",
+                            "value": "us"
+                        },
+                        {
+                            "key": "hl",
+                            "value": "en"
                         }
-                    },
-                    "condition": {
-                        "urlFilter": "google.com",
-                        "resourceTypes": [
-                            "main_frame"
-                        ]
-                    }
+                    ]
                 }
-            ]
+            }
         }
-    )
+    },
+    "condition": {
+        "urlFilter": "google.com",
+        "resourceTypes": [
+            "main_frame"
+        ],
+        "tabIds": [chrome.tabs.TAB_ID_NONE]
+    }
 }
 
-function disable(tabId) {
-    chrome.declarativeNetRequest.updateSessionRules(
-        {
-            "removeRuleIds": [1],
-            "addRules": [
-                {
-                    "id": 2,
-                    "action": {
-                        "type": "redirect",
-                        "redirect": {
-                            "transform": {
-                                "queryTransform": {
-                                    "removeParams": [
-                                        "gl",
-                                        "hl"
-                                    ]
-                                }
-                            }
-                        }
-                    },
-                    "condition": {
-                        "urlFilter": "google.com",
-                        "resourceTypes": [
-                            "main_frame"
-                        ]
-                    }
+
+rule_undo =
+{
+    "id": 1,
+    "action": {
+        "type": "redirect",
+        "redirect": {
+            "transform": {
+                "queryTransform": {
+                    "removeParams": ["gl", "hl"]
                 }
-            ]
+            }
         }
-    )
+    },
+    "condition": {
+        "urlFilter": "google.com",
+        "resourceTypes": [
+            "main_frame"
+        ]
+    }
 }
 
-toggle = false;
+
+chrome.declarativeNetRequest.updateSessionRules(
+    {
+        "removeRuleIds": [2],
+        "addRules": [rule]
+    }
+)
 
 chrome.action.onClicked.addListener(tab => {
-    toggle = !toggle;
-    if (toggle) {
-        f = enable
-        icon = "/images/icon_on.png"
-    } else {
-        f = disable
-        icon = "/images/icon_off.png"
-    }
-    chrome.action.setIcon({
-        path: icon
+    apply_rules = []
+    undo_flag = false;
+    chrome.declarativeNetRequest.getSessionRules(rules => {
+        rules.forEach(rule => {
+            tabIds = rule.condition.tabIds;
+            // already enabled -> disable
+            if (tabIds.includes(tab.id)) {
+                idx = tabIds.indexOf(tab.id);
+                rule.condition.tabIds.splice(idx, 1);
+                apply_rules.push(rule_undo);
+                undo_flag = true;
+            } else { // diabled -> enable
+                rule.condition.tabIds.push(tab.id);
+            }
+            apply_rules.push(rule);
+        })
+        // update rule
+        uprule = chrome.declarativeNetRequest.updateSessionRules(
+            {
+                "removeRuleIds": [2],
+                "addRules": apply_rules
+            }
+        )
     })
-    f(tab.id);
+
     chrome.tabs.reload(tab.id);
+
+    // not working
+    if (undo_flag) {
+        console.log('delete undo')
+        // delete undo rule
+        chrome.declarativeNetRequest.updateSessionRules(
+            {
+                "removeRuleIds": [1]
+            }
+        )
+    }
+
 });
