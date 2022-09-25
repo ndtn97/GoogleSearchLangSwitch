3443b298aa1c1d92c165e3a9ae07b16baa76131e
ndtn97 <32271862+ndtn97@users.noreply.github.com>
2022-09-26 00:45:38 +0900

    works

--- js/background.js
+++ js/background.js
@@ -52,6 +52,34 @@ rule_undo =
 }
 
 
+function updateIcon(tabId, url) {
+    if (url.match("google.com/search")) {
+        chrome.declarativeNetRequest.getSessionRules(rules => {
+            rules.forEach(rule => {
+                if (rule.id == 2) {
+                    tabIds = rule.condition.tabIds;
+                    if (tabIds.includes(tabId)) {
+                        // enabled
+                        icon = "/images/icon_on.png"
+                    } else {
+                        // diabled
+                        icon = "/images/icon_off.png"
+                    }
+
+                    chrome.action.setIcon({
+                        path: icon
+                    });
+                }
+            })
+        })
+    } else {
+        chrome.action.setIcon({
+            path: "/images/icon.png"
+        });
+    }
+}
+
+
 chrome.declarativeNetRequest.updateSessionRules(
     {
         "removeRuleIds": [2],
@@ -60,38 +88,50 @@ chrome.declarativeNetRequest.updateSessionRules(
 )
 
 chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
-    chrome.declarativeNetRequest.updateSessionRules(
-        {
-            "removeRuleIds": [1]
-        }
-    )
-});
-
-chrome.action.onClicked.addListener(tab => {
-    apply_rules = []
-    undo_flag = false;
-    chrome.declarativeNetRequest.getSessionRules(rules => {
-        rules.forEach(rule => {
-            tabIds = rule.condition.tabIds;
-            // already enabled -> disable
-            if (tabIds.includes(tab.id)) {
-                idx = tabIds.indexOf(tab.id);
-                rule.condition.tabIds.splice(idx, 1);
-                apply_rules.push(rule_undo);
-                undo_flag = true;
-            } else { // diabled -> enable
-                rule.condition.tabIds.push(tab.id);
-            }
-            apply_rules.push(rule);
-        })
-        // update rule
-        uprule = chrome.declarativeNetRequest.updateSessionRules(
+    // TODO too much exec ?
+    if (tab.url.match("google.com/search")) {
+        chrome.declarativeNetRequest.updateSessionRules(
             {
-                "removeRuleIds": [2],
-                "addRules": apply_rules
+                "removeRuleIds": [1]
             }
         )
-    })
+    }
+    updateIcon(tab.id, tab.url)
+});
 
-    chrome.tabs.reload(tab.id);
+chrome.tabs.onActivated.addListener(activeInfo => {
+    chrome.tabs.get(activeInfo.tabId, tab => {
+        updateIcon(activeInfo.tabId, tab.url)
+    })
+})
+
+chrome.action.onClicked.addListener(tab => {
+    if (tab.url.match("google.com/search")) {
+        chrome.declarativeNetRequest.getSessionRules(rules => {
+            apply_rules = []
+            rules.forEach(rule => {
+                tabIds = rule.condition.tabIds;
+                // already enabled -> disable
+                if (tabIds.includes(tab.id)) {
+                    idx = tabIds.indexOf(tab.id);
+                    rule.condition.tabIds.splice(idx, 1);
+                    apply_rules.push(rule_undo);
+                } else { // diabled -> enable
+                    rule.condition.tabIds.push(tab.id);
+                }
+                apply_rules.push(rule);
+            })
+
+            // update rule
+            chrome.declarativeNetRequest.updateSessionRules(
+                {
+                    "removeRuleIds": [2],
+                    "addRules": apply_rules
+                }
+            )
+
+            // reload
+            chrome.tabs.reload(tab.id);
+        })
+    }
 });
