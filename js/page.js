f7c03a5c09a9b939c53fee655f68c88591151191
ndtn97 <32271862+ndtn97@users.noreply.github.com>
2022-09-28 02:17:24 +0900

    add toggle btn

--- js/page.js
+++ js/page.js
@@ -1,1 +1,9 @@
+stats_html = document.getElementById("result-stats").innerHTML
+toggle_btn = `
+<span>EN <label class="cl-switch cl-switch-small cl-switch-green"><input type="checkbox"><span class="switcher">
+</span></label>&nbsp;</span>
+`
+
+document.getElementById("result-stats").innerHTML = toggle_btn + stats_html;
+
 chrome.runtime.sendMessage({ search: true })
